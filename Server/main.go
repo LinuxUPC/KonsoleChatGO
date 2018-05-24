1c0d830e87cdf782ce3c50b1bba7788bfe5e0f3c
johanitus1 <joan.vinyals@est.fib.upc.edu>
2018-05-24 20:17:08 +0200

    Minor server changes

--- Server/main.go
+++ Server/main.go
@@ -8,13 +8,36 @@ import (
 
 var users = make(map[string]net.Conn)
 
+func split(buffer [512] byte, size int) string[]
+	var splited []string
+	for pos := 0 {
+		actual := ""
+		dquoted:= buffer[pos] == '"'
+		if dquoted {
+			pos++
+		}
+		for i, char := range buffer[pos:]{
+			if 
+			pos++
+		}
+	}
+}
+
 func clientHandler(conn net.Conn){
 	defer conn.Close()
 	var buffer [512] byte
 	n, err := conn.Read(buffer[0:])
 	checkError(err)
-	fmt.Println("User", string(buffer[:n]), "logged in.")
-	conn.Write([]byte("y")) // don't care about return value
+	username := string(buffer[:n])
+	fmt.Println("User", username, "logged in.")
+	if users[username] == nil {
+		users[username] = conn
+	}
+	for {
+		n, err := conn.Read(buffer[0:])
+		checkError(err)
+		recived = split(buffer[:n], n)
+	}
 }
 
 func main() {
